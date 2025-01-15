742704508c9c4e2f5470efd6ade57e7c40c041db
wencaili <228295392@qq.com>
2025-01-15 17:59:48 +0800

    fix: image src

--- src/image.js
+++ src/image.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-// const axios = require("axios");
+const axios = require("axios");
 
 const rootFolder = path.join(__dirname, "../docs"); // 替换为你的根文件夹路径
 const downloadDir = path.join(__dirname, "../docs/image"); // 替换为你想要保存图片的目录路径
@@ -12,7 +12,9 @@ async function downloadAndReplaceImagesInMdFile(mdFilePath) {
     let mdContent = fs.readFileSync(mdFilePath, "utf8");
 
     // 正则表达式用于匹配MD文件中的http/https图片链接
-    const imgRegex = /!\[.*?\]\((https?:\/\/[^\s\)]+)\)/g;
+    // const imgRegex = /!\[.*?\]\((https?:\/\/[^\s\)]+)\)/g;
+    const imgRegex = /!\[.*?\]\(((https?:|\/\/)[^\s\)]+)\)/g;
+
 
     // 获取所有匹配的图片链接
     const imgMatches = mdContent.match(imgRegex);
@@ -36,7 +38,8 @@ async function downloadAndReplaceImagesInMdFile(mdFilePath) {
         const imgPath = path.join(downloadDir, imgName);
 
         // 发送HTTP请求下载图片
-        const response = await axios.get(imgSrc, {
+        const fullUrl = imgSrc.startsWith("//") ? `https:${imgSrc}` : imgSrc;
+        const response = await axios.get(fullUrl, {
           responseType: "arraybuffer",
         });
         fs.writeFileSync(imgPath, Buffer.from(response.data));
